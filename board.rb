54ce71df4f5faf717a1a3b997692bbbd41d18b5f
ponimole <onimolep@yahoo.com>
2020-01-12 16:28:14 -0600

    Created Board render method

--- board.rb
+++ board.rb
@@ -1,4 +1,5 @@
 require "./tile.rb"
+require "byebug"
 class Board
 
     def initialize(size)
@@ -10,6 +11,14 @@ class Board
             row.map do |tile|
                 tile = Tile.new
             end
+        end
+    end
+
+    def render
+        @grid.each do |row|
+            print_row = ""
+            row.each {|tile| print_row += tile.to_s + " " }
+            puts print_row[0...-1]
         end
     end
 end
