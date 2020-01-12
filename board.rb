f5b3a171288647b3b53af7ce2e8a150bc6e55eda
ponimole <onimolep@yahoo.com>
2020-01-12 15:29:51 -0600

    Added Board populatemethod

--- board.rb
+++ board.rb
@@ -1,7 +1,15 @@
+require "./tile.rb"
 class Board
 
     def initialize(size)
         @grid = Array.new(size) {Array.new(size)}
     end
 
+    def populate
+        @grid = @grid.map do |row|
+            row.map do |tile|
+                tile = Tile.new
+            end
+        end
+    end
 end
