310e595fe67d345c7e9c1050b1e7c79461dffcbd
ponimole <onimolep@yahoo.com>
2020-01-12 15:48:54 -0600

    Added simple Tile to_s method

--- tile.rb
+++ tile.rb
@@ -3,10 +3,17 @@ class Tile
     def initialize(mine = false)
         @mine = mine
         @hidden = true
+        @flagged = false
+        @fringed = false
     end
 
     def reveal
         @hidden = false
     end
 
+    def to_s
+        return "1" if @fringed
+        return "*" if @hidden
+        return "F" if @flagged
+    end
 end
